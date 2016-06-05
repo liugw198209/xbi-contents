a5d623699cab7eca25b841ee265be1e473157bd5
U-GMO\usr0101862 <usr0101862@JINN0781.GMO.LOCAL>
2016-06-05 15:07:02 +0900

    add tests

--- src/main/java/com/xbi/contents/mining/Article2VectorTraining.java
+++ src/main/java/com/xbi/contents/mining/Article2VectorTraining.java
@@ -4,9 +4,11 @@ import com.xbi.contents.mining.tools.DocItem;
 import com.xbi.contents.mining.tools.JapaneseTokenizerFactory;
 import com.xbi.contents.mining.tools.LoadDataFromDB;
 import com.xbi.contents.mining.tools.RowLabelAwareIterator;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
 import org.deeplearning4j.models.word2vec.wordstore.inmemory.InMemoryLookupCache;
 import org.deeplearning4j.text.documentiterator.LabelAwareIterator;
+import org.deeplearning4j.text.documentiterator.LabelledDocument;
 import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 import org.slf4j.Logger;
@@ -29,7 +31,8 @@ public class Article2VectorTraining {
     private static final Logger log = LoggerFactory.getLogger(Article2VectorTraining.class);
 
     public static void main(String[] args) throws Exception {
-        List<DocItem> rs = LoadDataFromDB.loadDataFromSqlite(null, null);
+        String inputSql = "select id, post_content from xb_corpus limit 100";
+        List<DocItem> rs = LoadDataFromDB.loadDataFromSqlite(null, inputSql);
         LabelAwareIterator iterator = new RowLabelAwareIterator.Builder()
                 .setRowSet(rs)
                 .build();
@@ -47,14 +50,14 @@ public class Article2VectorTraining {
         */
 
         ParagraphVectors vec = new ParagraphVectors.Builder()
-                .minWordFrequency(1)
-                .iterations(3)
-                .epochs(1)
+                .minWordFrequency(5)
+                .iterations(5)
+                .epochs(3)
                 .layerSize(100)
                 .learningRate(0.025)
                 .windowSize(5)
                 .iterate(iterator)
-                .trainWordVectors(false)
+                .trainWordVectors(true)
                 .vocabCache(cache)
                 .tokenizerFactory(t)
                 .sampling(0)
@@ -62,8 +65,28 @@ public class Article2VectorTraining {
 
         vec.fit();
 
-        double similarity1 = vec.similarity("DOC_9835", "DOC_12492");
-        log.info("9835/12492 similarity: " + similarity1);
+        log.info("Writing word vectors to text file....");
+
+        // Write word vectors
+        WordVectorSerializer.writeWordVectors(vec, "article2vec.txt");
+        //WordVectorSerializer.writeFullModel(vec, "fullmodel.txt");
+
+        iterator.reset();
+        LabelledDocument doc1 = iterator.nextDocument();
+        LabelledDocument doc2 = iterator.nextDocument();
+
+        //docId is a word. vec contain all words and docIds
+        //Two types of similarity: (1) trained vectors if given labels (docId), (2) mean vectors if given doc contents
+        double sim1 = vec.similarityToLabel(doc1, doc1.getLabel());
+        double sim2 = vec.similarity(doc1.getLabel(), doc2.getLabel());
+        double[] sim3 = vec.getWordVector(doc1.getLabel());
+        //double sim4 = vec.similarityToLabel(doc1.getReferencedContent(), doc2.getLabel());
+        double sim5 = vec.similarityToLabel(doc1.getContent(), doc2.getLabel());
+        double sim6 = vec.similarityToLabel(doc2.getLabel(), doc2.getLabel());
+
+        log.info(sim1 + "," + sim2 + "," + sim5 + "," + sim6);
+        //double similarity1 = vec.similarity("DOC_9835", "DOC_12492");
+        //log.info("9835/12492 similarity: " + similarity1);
 
     }
 }
