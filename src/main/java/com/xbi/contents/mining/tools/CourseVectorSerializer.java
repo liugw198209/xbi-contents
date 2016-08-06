c2ec68aa6d11c00e62d4019f9413627046be54a9
U-GMO\usr0101862 <usr0101862@JINN0781.GMO.LOCAL>
2016-08-06 23:05:22 +0900

    save course vectors to db

--- src/main/java/com/xbi/contents/mining/tools/CourseVectorSerializer.java
+++ src/main/java/com/xbi/contents/mining/tools/CourseVectorSerializer.java
@@ -1,11 +1,17 @@
 package com.xbi.contents.mining.tools;
 
+import org.canova.api.records.reader.RecordReader;
+import org.canova.api.records.reader.impl.CSVRecordReader;
 import org.canova.api.split.FileSplit;
+import org.canova.api.writable.Writable;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,7 +27,7 @@ public class CourseVectorSerializer {
     private static DataSetIterator fullDataSet = null;
 
     public static void main(String[] args) throws Exception {
-        new CourseVectorSerializer().loadCourseVectors();
+        saveCourseVectorsToDB("course2vec.txt");
     }
 
     public static DataSetIterator loadCourseVectors(){
@@ -69,5 +75,60 @@ public class CourseVectorSerializer {
 
     public static void setLabelIds(HashMap<String, Integer> labelIds) {
         CourseVectorSerializer.labelIds = labelIds;
+    }
+
+    public static void saveCourseVectorsToDB(String filePath){
+        String query = "INSERT INTO le_scourse_vector(course_id, create_datetime, update_datetime, vectors) VALUES (?, now(), now(), ?)";
+
+        RecordReader rr = new CSVRecordReader(0, " ");
+
+        try {
+            rr.initialize(new FileSplit(new File(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Connection connection = null;
+
+        try{
+            connection = DBManager.getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+
+            int cnt = 0;
+            while(rr.hasNext()){
+
+                int col = 0;
+                StringBuilder sb = new StringBuilder();
+                for(Writable w : rr.next()){
+                    if(col == 1) ps.setLong(1, w.toLong());
+                    if(col > 1) sb.append(String.format("%.8f ", w.toDouble()));
+                    col++;
+                }
+                if(col > 1) ps.setString(2, sb.substring(0, sb.length()-1));
+                cnt++;
+
+                ps.addBatch();
+
+                if (cnt % 1000 == 0 ) {
+                    ps.executeBatch(); // Execute every 1000 items.
+                }
+            }
+            if(cnt % 1000 != 0)
+                ps.executeBatch();
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
